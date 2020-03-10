cde541490beeb3446d24a41332b369b9e6a12827
Octoloco <diego_clorio@hotmail.com>
2020-03-09 20:16:55 -0600

    Almost Finished Parry
    Missing the timer to limit the parry for the player.

---
+++ ParryGame/Assets/Diego/Scripts/Parry.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parry : MonoBehaviour
+{
+    public bool upDir;
+    public bool downDir;
+    public bool leftDir;
+    public bool rightDir;
+
+    public List<GameObject> bulletsEnetered;
+   
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        bulletsEnetered = new List<GameObject>();
+
+        bulletsEnetered.Add(other.gameObject);
+
+        foreach (GameObject b in bulletsEnetered) {
+
+            if (other.tag == "Bullet")
+            {
+                if (upDir)
+                {
+                    other.transform.rotation = Quaternion.Euler(0, 0, -90);
+                    upDir = false;
+                    other.tag = "ParriedBullet";
+                    GetComponent<CircleCollider2D>().enabled = false;
+                }
+                else if (downDir)
+                {
+                    
+                    other.transform.rotation = Quaternion.Euler(0, 0, 90);
+                    downDir = false;
+                    other.tag = "ParriedBullet";
+                    GetComponent<CircleCollider2D>().enabled = false;
+                }
+                else if (leftDir)
+                {
+                    other.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    leftDir = false;
+                    other.tag = "ParriedBullet";
+                    GetComponent<CircleCollider2D>().enabled = false;
+                }
+                else if (rightDir)
+                {
+                    other.transform.rotation = Quaternion.Euler(0, 0, 180);
+                    rightDir = false;
+                    other.tag = "ParriedBullet";
+                    GetComponent<CircleCollider2D>().enabled = false;
+                }
+            }
+
+            
+        }
+    }
+
+}
