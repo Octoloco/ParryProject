76b4e8befc020aa8eb1001bf321a026ea33706f3
Diego Clorio <diego_clorio@hotmail.com>
2021-09-19 08:16:17 -0500

    Breakable platform now respawns

--- ParryGame/Assets/Carlos/Scripts/DestructiblePlatform.cs
+++ ParryGame/Assets/Carlos/Scripts/DestructiblePlatform.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class DestructiblePlatform : MonoBehaviour
 {
     [SerializeField] float timeToDestroy;
+    [SerializeField] float timeToRespawn;
+
+    private bool breaking;
 
     IEnumerator Shake()
     {
@@ -20,11 +23,11 @@ public class DestructiblePlatform : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision");
-        if (collision.transform.tag == "Player")
+        if (collision.transform.tag == "Player" && !breaking)
         {
+            breaking = true;
             StartCoroutine(Shake());
             Invoke("Kill", timeToDestroy);
-            
         }
     }
 
@@ -34,6 +37,16 @@ public class DestructiblePlatform : MonoBehaviour
         {
             GetComponentInChildren<Player_Controller>().gameObject.transform.SetParent(null);
         }
-        Destroy(gameObject);
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+        StopAllCoroutines();
+        Invoke("Respawn", timeToRespawn);
+    }
+
+    private void Respawn()
+    {
+        breaking = false;
+        GetComponent<BoxCollider2D>().enabled = true;
+        GetComponent<SpriteRenderer>().enabled = true;
     }
 }
