a8db757c8219542a23127b6fbd72eb36d27f7d24
Diego Clorio <diego_clorio@hotmail.com>
2021-09-18 19:29:54 -0500

    Main menu and transitions

--- ParryGame/Assets/Carlos/Scripts/UI/SoundSelectionUI.cs
+++ ParryGame/Assets/Carlos/Scripts/UI/SoundSelectionUI.cs
@@ -7,18 +7,17 @@ public class SoundSelectionUI : MonoBehaviour
 {
     [SerializeField] Slider slider;
     [SerializeField] SoundCollection collection;
-    [SerializeField] Text visualFeedback;
+    //[SerializeField] Text visualFeedback;
     // Start is called before the first frame update
     void Awake()
     {
-        collection.SetVolume(0.5f);
-        slider.value = 0.5f;
-        visualFeedback.text = ((int)(slider.value * 100)).ToString();
+        slider.value = collection.volume;
+        //visualFeedback.text = ((int)(slider.value * 100)).ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        visualFeedback.text = ((int)(slider.value*100)).ToString();
+        //visualFeedback.text = ((int)(slider.value*100)).ToString();
     }
 }
